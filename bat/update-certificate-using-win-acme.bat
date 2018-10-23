a9b1421eb9cfb91efe880a7d01330b1726217ea6
E.J. Sexton <ejsexton82@gmail.com>
2018-10-23 17:31:18 +0200

    Added confirmation flags

--- bat/update-certificate-using-win-acme.bat
+++ bat/update-certificate-using-win-acme.bat
@@ -2,10 +2,10 @@ REM Set the script parameter to "{0}"
 SET host=%1
 
 REM Remove the existing certificate
-fmsadmin certificate delete
+fmsadmin certificate delete --yes
 
 REM Install the new certificate
-fmsadmin certificate import --keyfile C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-key.pem --intermediateCA C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\ca-%host%-crt.pem C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-crt.pem
+fmsadmin certificate import --yes --keyfile C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-key.pem --intermediateCA C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\ca-%host%-crt.pem C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-crt.pem
 
 REM Restart FileMaker Server
 net stop "FileMaker Server"
