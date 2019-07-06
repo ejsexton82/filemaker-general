eb7c6a636cb9c52dc8084bfb27ba63ce8c7178e6
E.J. Sexton <ejsexton82@gmail.com>
2019-07-06 11:30:45 -0500

    Committing outstanding changes

--- bat/update-certificate-using-win-acme.bat
+++ bat/update-certificate-using-win-acme.bat
@@ -1,12 +1,12 @@
-REM Set the script parameter to "{0}"
-SET host=%1
+REM Set the script parameter to "{CacheFile}"
+SET file=%1
 
 REM Remove the existing certificate
-fmsadmin certificate delete --yes
+REM fmsadmin certificate delete --yes
 
 REM Install the new certificate
-fmsadmin certificate import --yes --keyfile C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-key.pem --intermediateCA C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\ca-%host%-crt.pem C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-crt.pem
+REM fmsadmin certificate import --yes --keyfile C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-key.pem --intermediateCA C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\ca-%host%-crt.pem C:\ProgramData\win-acme\httpsacme-v01.api.letsencrypt.org\%host%-crt.pem
 
 REM Restart FileMaker Server
-net stop "FileMaker Server"
-net start "FileMaker Server"
+REM net stop "FileMaker Server"
+REM net start "FileMaker Server"
